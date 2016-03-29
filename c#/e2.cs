9725f94a45f9afd3922565460b52368db38ded1a
tomaziniale <tomazini.alexandre@gmail.com>
2016-03-29 19:22:06 -0300

    Update e2.cs

--- c#/e2.cs
+++ c#/e2.cs
@@ -11,10 +11,10 @@ public void e2 ()
     
   for(int i=1; i<4000000; i++)
   {
-      fib2 = fib1;
-		  fib1 = fib;
-		  fib = fib1+fib2;
-		  i = fib; 
+	fib2 = fib1;
+	fib1 = fib;
+	fib = fib1+fib2;
+	i = fib; 
     	if (fib % 2 == 0) sum += fib; 
   }
   
