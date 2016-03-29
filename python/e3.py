ba10c5c51cd17155943bdec7a2094509eea17eac
tomazini.alexandre@gmail.com <tomazini.alexandre@gmail.com>
2016-03-29 20:52:47 -0300

    Largest prime factor

---
+++ python/e3.py
@@ -0,0 +1,26 @@
+# Largest prime factor
+
+def isPrime(x):
+    divided = 1;
+    for i in range(1,x):
+        if x % i == 0:
+            divided += 1;
+        if divided <= 2:
+            return True;
+	
+    return False;
+
+def getDivisors(x):
+    i = 1;
+    divisors = [];
+    while i <= x:        
+        if isPrime(i):
+            if x % i == 0:
+                divisors.append(i);                
+                x = x / i;
+        i += 1;
+    
+    return divisors;
+
+print(getDivisors(13195));
+print(getDivisors(600851475143));
